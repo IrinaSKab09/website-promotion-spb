825f7f21475546770182aacb49afb1223001234c
Irina <55109138+IrinaSKab@users.noreply.github.com>
2020-04-02 13:51:43 +0300

    feat: add section price, phpmailer, thanks.html, send.php

--- js/main.js
+++ js/main.js
@@ -4,4 +4,25 @@ var menu = document.querySelector('.menu');
 menuButton.addEventListener('click', function() {
   menuButton.classList.toggle('menu__button-active');
   menu.classList.toggle('menu-active');
+
 });
+
+new WOW().init();
+
+var player;
+
+$('.video__icon').on('click', function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '100%',
+    width: '100%',
+    videoId: 'pLPX8jiY0Pk',
+    events: {
+      'onReady': videoPlay,
+    }
+  });
+})
+  
+function videoPlay(event) {
+  event.target.playVideo();
+}
+
